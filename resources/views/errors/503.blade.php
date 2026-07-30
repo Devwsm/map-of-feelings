53abc2238198606764e652abc16ead228afc09cc
arga <it.wsm2026@gmail.com>
2026-07-30 10:07:17 +0700

    maintenance mode

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -5,7 +5,8 @@
         <div class="mof-intro-color-field" aria-hidden="true"></div>
         <div class="relative z-2 w-[min(850px,82vw)]">
             <img src="{{ asset('assets/logo/logo-map-of-feelings.svg') }}" alt="Map of Feelings" class="block w-full h-auto" />
-            <p class="font-mono tracking-[.22em] mt-7 text-3xl sm:text-5xl font-semibold">27 AGUSTUS 2026</p>
+            <p class="sm:mb-4 text-sm sm:text-xl font-semibold">an album by Whisnu Santika</p>
+            <p class="font-mono tracking-[.22em] text-2xl sm:text-5xl font-semibold">27 AGUSTUS 2026</p>
         </div>
     </section>
 @endsection
