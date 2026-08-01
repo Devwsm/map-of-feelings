44c142c1210cbbb081c66f27f3ab817b5eabd162
arga <it.wsm2026@gmail.com>
2026-08-01 19:47:14 +0700

    moods seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             pressconGuestSeeder::class,
             userSeeder::class,
+            moodSeeder::class,
         ]);
     }
 }
