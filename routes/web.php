0ff1da704fcdb47018468be844704963efcc49c7
arga <it.wsm2026@gmail.com>
2026-08-02 19:12:53 +0700

    best practice route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\checkinController;
 use App\Http\Controllers\dashboardController;
 use App\Http\Controllers\homeController;
 use App\Http\Controllers\loginController;
@@ -17,27 +18,30 @@ Route::prefix('/')->group(function () {
     Route::post('/logout', [loginController::class, 'destroy'])->name('logout');
 });
 
-Route::middleware('auth')->group(function () {
+Route::prefix('/dashboard')->middleware('auth')->group(function () {
     // Admin & staff berdua boleh
     Route::middleware('role:admin,staff')->group(function () {
-        Route::get('/dashboard', [dashboardController::class, 'index'])->name('dashboard');
-        // Route::get('/dashboard/checkin', [...])->name('dashboard.checkin');
-    });
-    
-    // Cuma admin
-    Route::middleware('role:admin')->group(function () {
-        Route::get('/dashboard/tamu', [pressconGuestController::class, 'index'])->name('dashboard.tamu');
-        Route::get('/dashboard/tamu/create', [pressconGuestController::class, 'create'])->name('dashboard.tamu.create');
-        Route::post('/dashboard/tamu', [pressconGuestController::class, 'store'])->name('dashboard.tamu.store');
-        Route::get('/dashboard/tamu/{guest}/edit', [pressconGuestController::class, 'edit'])->name('dashboard.tamu.edit');
-        Route::put('/dashboard/tamu/{guest}', [pressconGuestController::class, 'update'])->name('dashboard.tamu.update');
-        Route::delete('/dashboard/tamu/{guest}', [pressconGuestController::class, 'destroy'])->name('dashboard.tamu.destroy');
+        Route::get('/', [dashboardController::class, 'index'])->name('dashboard');
+        Route::get('/checkin', [checkinController::class, 'index'])->name('dashboard.checkin');
+        Route::get('/checkin/search', [checkinController::class, 'search'])->name('dashboard.checkin.search');
+        Route::post('/checkin/{guest}', [checkinController::class, 'store'])->name('dashboard.checkin.store');
     });
 
+    // Cuma admin
+    Route::middleware('role:admin')->group(function () {
+        Route::get('/tamu', [pressconGuestController::class, 'index'])->name('dashboard.tamu');
+        Route::get('/tamu/create', [pressconGuestController::class, 'create'])->name('dashboard.tamu.create');
+        Route::post('/tamu', [pressconGuestController::class, 'store'])->name('dashboard.tamu.store');
+        Route::get('/tamu/{guest}/edit', [pressconGuestController::class, 'edit'])->name('dashboard.tamu.edit');
+        Route::put('/tamu/{guest}', [pressconGuestController::class, 'update'])->name('dashboard.tamu.update');
+        Route::delete('/tamu/{guest}', [pressconGuestController::class, 'destroy'])->name('dashboard.tamu.destroy');
+    });
+
+    // Cuma panel
     Route::middleware('role:panel')->group(function () {
-        Route::get('/dashboard/panel', [homeController::class, 'panel'])->name('dashboard.panel');
-        Route::get('/dashboard/panel/{mood}/edit', [homeController::class, 'editPanel'])->name('dashboard.panel.edit');
-        Route::put('/dashboard/panel/{mood}', [homeController::class, 'updatePanel'])->name('dashboard.panel.update');
+        Route::get('/panel', [homeController::class, 'panel'])->name('dashboard.panel');
+        Route::get('/panel/{mood}/edit', [homeController::class, 'editPanel'])->name('dashboard.panel.edit');
+        Route::put('/panel/{mood}', [homeController::class, 'updatePanel'])->name('dashboard.panel.update');
     });
 });
 
